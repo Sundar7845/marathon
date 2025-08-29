a7776179a6b0e942bfb3debf0626d7c364877517
sundar7854 <prathip221@gmail.com>
2025-08-29 11:08:16 +0530

    db updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('mobile');
+            $table->string('category');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
